993b74cb3e3eb064c9bfcdcdb9d402e379108051
umararrosyad <umararrosyad@gmail.com>
2025-05-25 18:55:45 +0700

    ix: correct namespace for RoomUsersController"

--- app/controllers/api/v1/room_users_controller.rb
+++ app/controllers/api/v1/room_users_controller.rb
@@ -1,14 +1,21 @@
-class RoomUsersController < ApplicationController
-  def create
-    room = Room.find(params[:room_id])
-    user_ids = params[:user_ids] # array of UUID
-
-    user_ids.each do |uid|
-      RoomUser.find_or_create_by!(room_id: room.id, user_id: uid) do |ru|
-        ru.joined_at = Time.current
-      end
+module Api
+    module V1
+        class RoomUsersController < ApplicationController
+            # aksi-aksi controller di sini
+        class RoomUsersController < ApplicationController
+            def create
+            room = Room.find(params[:room_id])
+            user_ids = params[:user_ids] # array of UUID
+        
+            user_ids.each do |uid|
+                RoomUser.find_or_create_by!(room_id: room.id, user_id: uid) do |ru|
+                ru.joined_at = Time.current
+                end
+            end
+        
+            render json: { message: "Users joined" }, status: :ok
+            end
+        end
     end
-
-    render json: { message: "Users joined" }, status: :ok
   end
 end
