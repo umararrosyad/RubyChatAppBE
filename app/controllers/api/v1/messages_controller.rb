e0c4dd6f652b490137524b633141663b550d701b
umararrosyad <umararrosyad@gmail.com>
2025-05-25 20:38:21 +0700

    sen message error

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -2,33 +2,43 @@ module Api
   module V1
     class MessagesController < ApplicationController
       def create
-        message = Message.new(message_params.except(:user_nickname))
+        begin
+          message = Message.new(message_params.except(:user_nickname))
 
-        if message.save
-          ActionCable.server.broadcast("chat_room_#{message.room_id}", {
-            id: message.id,
-            content: message.content,
-            user_id: message.user_id,
-            user_nickname: message_params[:user_nickname],
-            created_at: message.created_at.strftime("%H:%M")
-          })
-
-          render json: {
-            success: true,
-            message: "Message sent successfully",
-            data: {
+          if message.save
+            ActionCable.server.broadcast("chat_room_#{message.room_id}", {
               id: message.id,
               content: message.content,
               user_id: message.user_id,
               user_nickname: message_params[:user_nickname],
               created_at: message.created_at.strftime("%H:%M")
-            }
-          }, status: :created
-        else
+            })
+
+            render json: {
+              success: true,
+              message: "Message sent successfully",
+              data: {
+                id: message.id,
+                content: message.content,
+                user_id: message.user_id,
+                user_nickname: message_params[:user_nickname],
+                created_at: message.created_at.strftime("%H:%M")
+              }
+            }, status: :created
+          else
+            render json: {
+              success: false,
+              error: message.errors.full_messages
+            }, status: :unprocessable_entity
+          end
+
+        rescue => e
           render json: {
             success: false,
-            error: message.errors.full_messages
-          }, status: :unprocessable_entity
+            error: "Internal Server Error",
+            detail: e.message,
+            location: e.backtrace.first # Menampilkan file dan baris error pertama
+          }, status: :internal_server_error
         end
       end
 
