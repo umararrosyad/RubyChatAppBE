8a2fa424bb2967126db9b58849c3ce0f4834141d
umararrosyad <umararrosyad@gmail.com>
2025-05-25 20:33:54 +0700

    update send message

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -2,52 +2,40 @@ module Api
   module V1
     class MessagesController < ApplicationController
       def create
-        begin
-          message = Message.new(message_params)
+        message = Message.new(message_params.except(:user_nickname))
 
-          if message.save
-            ActionCable.server.broadcast("chat_room_#{message.room_id}", {
+        if message.save
+          ActionCable.server.broadcast("chat_room_#{message.room_id}", {
+            id: message.id,
+            content: message.content,
+            user_id: message.user_id,
+            user_nickname: message_params[:user_nickname],
+            created_at: message.created_at.strftime("%H:%M")
+          })
+
+          render json: {
+            success: true,
+            message: "Message sent successfully",
+            data: {
               id: message.id,
               content: message.content,
               user_id: message.user_id,
-              user_nickname: message.user&.nickname,
+              user_nickname: message_params[:user_nickname],
               created_at: message.created_at.strftime("%H:%M")
-            })
-
-            render json: {
-              success: true,
-              message: "Message sent successfully",
-              data: {
-                id: message.id,
-                content: message.content,
-                user_id: message.user_id,
-                user_nickname: message.user&.nickname,
-                created_at: message.created_at.strftime("%H:%M")
-              }
-            }, status: :created
-          else
-            render json: {
-              success: false,
-              error: message.errors.full_messages
-            }, status: :unprocessable_entity
-          end
-        rescue => e
-          # Log error ke server dan tampilkan error sebagai JSON (untuk debugging)
-          Rails.logger.error("Error in MessagesController#create: #{e.message}")
-          Rails.logger.error(e.backtrace.join("\n"))
-
+            }
+          }, status: :created
+        else
           render json: {
             success: false,
-            error: "Internal Server Error",
-            detail: e.message
-          }, status: :internal_server_error
+            error: message.errors.full_messages
+          }, status: :unprocessable_entity
         end
       end
 
       private
 
       def message_params
-        params.require(:message).permit(:room_id, :user_id, :content)
+        params.require(:message).permit(:room_id, :user_id, :content, :user_nickname)
       end
     end
   end
