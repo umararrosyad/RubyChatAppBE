b5d469cae1059e7764b7a11ccefd62e23571255b
umararrosyad <umararrosyad@gmail.com>
2025-06-17 14:45:12 +0700

    fix development cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://vue-chat-app-sft1.vercel.app"
+    origins "*"
 
     resource "*",
       headers: :any,
